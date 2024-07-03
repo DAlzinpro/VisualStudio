ee185dd5401dbcbf0e548e05255d8b426315b366
DAlzinpro <101902234+DAlzinpro@users.noreply.github.com>
2024-07-03 09:09:08 -0300

    Add files via upload

---
+++ redtube/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace redtube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            panelopt.Visible = false;
+
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            panelopt.Visible = true;
+
+        }
+    }
+}
